1f34460b72ebbc553312ba9dfb1a4d683b1f0945
BorisR <borisrabin@users.noreply.github.com>
2015-11-04 20:04:40 -0500

    Update grayLogclient.sh

--- grayLogclient.sh
+++ grayLogclient.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 IP_ADDR='38.99.188.17'
 
-cat > /etc/fail2ban/jail.local <<EOF
-/etc/rsyslog.d/90-graylog2.conf
+cat > /etc/rsyslog.d/90-graylog2.conf <<EOF
 $template GRAYLOGRFC5424,"%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msg%\n"
 *.* @$IP_ADDR:514;GRAYLOGRFC5424
 EOF
