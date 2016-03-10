5f1864c7d2b05131938d72937bc8ea6193821935
BorisR <borisrabin@users.noreply.github.com>
2016-03-10 12:21:24 -0500

    Update grayLogclient.sh

--- grayLogclient.sh
+++ grayLogclient.sh
@@ -3,7 +3,7 @@ IP_ADDR='38.99.188.17'
 firewall-cmd --zone=public --add-port=514/udp --permanent
 firewall-cmd --reload
 cat > /etc/rsyslog.d/90-graylog2.conf <<EOF
-$template GRAYLOGRFC5424,"%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msg%\n"
+$template GRAYLOGRFC5424,"<%PRI%>%PROTOCOL-VERSION% %TIMESTAMP:::date-rfc3339% %HOSTNAME% %APP-NAME% %PROCID% %MSGID% %STRUCTURED-DATA% %msg%\n"
 *.* @$IP_ADDR:514;GRAYLOGRFC5424
 EOF
 service rsyslog restart
